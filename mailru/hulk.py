bb79cd4a1aaaa90bbe2729a5953fc7dd6dcf3a96
Kirill Proskurin <oloremo@gmail.com>
2015-09-16 15:23:41 +0300

    add try - except, add signal handler to catch wierd shit

--- mailru/hulk.py
+++ mailru/hulk.py
@@ -10,6 +10,7 @@ import fcntl
 import errno
 import tarfile
 import logging
+import signal
 from sys import exit
 from os.path import isfile, isdir
 from optparse import OptionParser
@@ -53,6 +54,10 @@ backup_time = strftime('%d.%m.%Y_%H%M', localtime())
 error_file = '/var/tmp/hulk-'
 loglevel = logging.getLevelName(opts.loglevel)
 
+
+def sig_handler(signum=None, frame=None):
+    logger.critical('Caught signal "%s"' % signum)
+    exit(0)
 
 def load_config(file, type):
     logger.info('Loading config file "%s"' % file)
@@ -182,7 +187,7 @@ def make_tarfile(output_filename, source_dir):
     tar = tarfile.open(output_filename, "w")
     tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-def cleanup(inst, type):
+def cleanup(inst, mode):
     oldest_snap = sorted(os.listdir(inst['base_dir'] + '/snaps'))[-1] if os.listdir(inst['base_dir'] + '/xlogs') else ''
     oldest_xlog = sorted(os.listdir(inst['base_dir'] + '/xlogs'))[-1] if os.listdir(inst['base_dir'] + '/xlogs') else ''
     oldest_snap_lsn = int(oldest_snap.split('.')[0]) if oldest_snap else 0
@@ -195,75 +200,78 @@ def cleanup(inst, type):
         limit_ut = limit * 86400
         time_type = 'days'
 
-    if type == 'snaps':
-        base_dir = inst['base_dir'] + '/' + type
+    if mode == 'snaps':
+        base_dir = inst['base_dir'] + '/' + mode
         for file in [file for file in os.listdir(base_dir) if file != oldest_snap]:
             fullpath = base_dir + '/' + file
             if os.lstat(fullpath).st_mtime < now - limit_ut:
                 logger.info('Deleting %s, older than %s %s' % (fullpath, limit, time_type))
                 os.unlink(fullpath)
-    if type == 'xlogs':
-        base_dir = inst['base_dir'] + '/' + type
+    if mode == 'xlogs':
+        base_dir = inst['base_dir'] + '/' + mode
         for file in [file for file in os.listdir(base_dir) if int(file.split('.')[0]) < oldest_snap_lsn and file != oldest_xlog]:
             fullpath = base_dir + '/' + file
             if os.lstat(fullpath).st_mtime < now - limit_ut:
                 logger.info('Deleting %s, older than %s %s' % (fullpath, limit, time_type))
                 os.unlink(fullpath)
 
-def backup(inst_dict, type, hostname, global_tmpdir, timeout):
+def backup(inst_dict, mode, hostname, global_tmpdir, timeout):
 
     for inst in inst_dict:
         inst_name = inst['base_dir'].split('/')[-1]
-        module_path = '%s/%s/%s/%s' % (inst['type'], hostname, inst_name, type)
+        module_path = '%s/%s/%s/%s' % (inst['type'], hostname, inst_name, mode)
 
-        if type == 'xdata' and inst['type'] == 'silver' and inst['skip_backup'] == 0:
-            base_dir = inst['base_dir'] + '/xdata'
-            xdata_dirs = [file for file in os.listdir(base_dir) if isdir(base_dir + '/' + file)]
-            tars = {}
-            create_rsync_dirs(inst, hostname, type)
+        try:
+            if mode == 'xdata' and inst['type'] == 'silver' and inst['skip_backup'] == 0:
+                base_dir = inst['base_dir'] + '/xdata'
+                xdata_dirs = [file for file in os.listdir(base_dir) if isdir(base_dir + '/' + file)]
+                tars = {}
+                create_rsync_dirs(inst, hostname, mode)
 
-            for xdata in xdata_dirs:
-                tars[xdata] = []
-                xdata_base_dir = base_dir + '/' + xdata
-                tmpdir = global_tmpdir if global_tmpdir else xdata_base_dir + '/backup_temp'
-                tar_name = '%s/walroot%s.tar' % (tmpdir, xdata)
-                if not os.path.exists(tmpdir):
-                    os.makedirs(tmpdir)
-                else:
-                    ### FIXME
+                for xdata in xdata_dirs:
+                    tars[xdata] = []
+                    xdata_base_dir = base_dir + '/' + xdata
+                    tmpdir = global_tmpdir if global_tmpdir else xdata_base_dir + '/backup_temp'
+                    tar_name = '%s/walroot%s.tar' % (tmpdir, xdata)
+                    if not os.path.exists(tmpdir):
+                        os.makedirs(tmpdir)
+                    else:
+                        rmtree(tmpdir)
+                        os.makedirs(tmpdir)
+                    for root, dirs, files in os.walk(xdata_base_dir, followlinks=True):
+                        if 'backup_temp' not in root and xdata_base_dir != root:
+                            os.makedirs(tmpdir + root)
+                        if root and 'backup_temp' not in root and xdata_base_dir != root:
+                            for file in files:
+                                fullpath = root + '/' + file
+                                tmp_fullpath = tmpdir + root + '/' + file
+                                copy_file(fullpath, tmp_fullpath, timeout)
+                    logger.info("Making tar for %s as %s" % (xdata_base_dir, tar_name))
+                    make_tarfile(tar_name, tmpdir)
+                    tars[xdata].append(tar_name)
+
+                for xdata in tars.keys():
+                    tmpdir = global_tmpdir if global_tmpdir else base_dir + '/' + xdata + '/backup_temp'
+                    rsync_files(base_dir, tars[xdata], False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'], add_timestamp=True)
+                    logger.info("Deleting temp dir: %s" % tmpdir)
                     rmtree(tmpdir)
-                    os.makedirs(tmpdir)
-                for root, dirs, files in os.walk(xdata_base_dir, followlinks=True):
-                    if 'backup_temp' not in root and xdata_base_dir != root:
-                        os.makedirs(tmpdir + root)
-                    if root and 'backup_temp' not in root and xdata_base_dir != root:
-                        for file in files:
-                            fullpath = root + '/' + file
-                            tmp_fullpath = tmpdir + root + '/' + file
-                            copy_file(fullpath, tmp_fullpath, timeout)
-                logger.info("Making tar for %s as %s" % (xdata_base_dir, tar_name))
-                make_tarfile(tar_name, tmpdir)
-                tars[xdata].append(tar_name)
 
-            for xdata in tars.keys():
-                tmpdir = global_tmpdir if global_tmpdir else base_dir + '/' + xdata + '/backup_temp'
-                rsync_files(base_dir, tars[xdata], False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'], add_timestamp=True)
-                logger.info("Deleting temp dir: %s" % tmpdir)
-                rmtree(tmpdir)
+            elif mode == 'snaps' or mode == 'xlogs':
+                if inst['skip_backup'] == 0:
+                    create_rsync_dirs(inst, hostname, mode)
+                    backupdir = '%s/%s/' % (inst['base_dir'], mode)
+                    rsync_files(backupdir, False, False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'])
+                    cleanup(inst, mode)
+                else:
+                    cleanup(inst, mode)
 
-        elif type == 'snaps' or type == 'xlogs':
-            if inst['skip_backup'] == 0:
-                create_rsync_dirs(inst, hostname, type)
-                backupdir = '%s/%s/' % (inst['base_dir'], type)
-                rsync_files(backupdir, False, False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'])
-                cleanup(inst, type)
-            else:
-                cleanup(inst, type)
-
-        elif type == 'other' and inst['optfile_list'] != '' and inst['skip_backup'] == 0:
-            create_rsync_dirs(inst, hostname, type)
-            files = inst['optfile_list'].split(',')
-            rsync_files(False, files, False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'], add_timestamp=True)
+            elif mode == 'other' and inst['optfile_list'] != '' and inst['skip_backup'] == 0:
+                create_rsync_dirs(inst, hostname, mode)
+                files = inst['optfile_list'].split(',')
+                rsync_files(False, files, False, inst['rsync_opts'], inst['rsync_host'], inst['rsync_login'], module_path, inst['rsync_passwd'], inst['type'], add_timestamp=True)
+        except Exception as e:
+            logger.critical('Error occured. Type was: "%s", mode was "%s"') % (inst['type'], mode)
+            logger.exception(e)
 
 ###Logger init
 logname = "%s %s" % (opts.type.upper(), opts.mode.upper())
@@ -297,6 +305,9 @@ logger.info('Started')
 fqdn = (socket.getfqdn())
 short = fqdn.split('.')[0]
 hostname = short + '.i'
+
+for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
+    signal.signal(sig, sig_handler)
 
 inst_dict = get_conf(opts.config, 'silver', hostname)
 backup(inst_dict, opts.mode, short, opts.tmpdir, opts.timeout)
